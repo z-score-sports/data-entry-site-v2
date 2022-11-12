01bbd659cbaba25c580f1ec5089a5356886e2f12
Michael Djorup <michael.djorup@gmail.com>
2022-11-12 10:37:58 -0500

    comments and visibility edits

--- src/state/actions/FreeThrow.ts
+++ src/state/actions/FreeThrow.ts
@@ -4,10 +4,10 @@ import { Action } from "./Action";
 
 class FreeThrow extends Action {
 
-    shootingPlayer:Player;
-    made : boolean = false;
+    public shootingPlayer:Player;
+    public made : boolean = false;
 
-    constructor(shootingPlayer:Player, made:boolean) {
+    public constructor(shootingPlayer:Player, made:boolean) {
         super();
         this.shootingPlayer = shootingPlayer;
         this.made = made;
@@ -20,9 +20,13 @@ class FreeThrow extends Action {
 
     }
 
-    editMade(newMade:boolean) {
+    public editMade(newMade:boolean) {
+        //case when the made boolean isn't changed
         if(this.made == newMade){return};
-
+        //change the made
+        this.made = newMade;
+        //miss to made -> add free throw made
+        //made to miss -> remove free throw made
         if(newMade == true && this.made == false) {
             this.shootingPlayer.addFreeThrowMade();
         } else {
@@ -31,25 +35,29 @@ class FreeThrow extends Action {
 
     }
 
-    editPlayer(newPlayer:Player) {
+    public editPlayer(newPlayer:Player) {
+        //case when players are the same
+        if(this.shootingPlayer == newPlayer){return;}
+        //first remove the stats from the current player
         this.shootingPlayer.removeFreeThrowAttempt();
         if(this.made){
             this.shootingPlayer.removeFreeThrowMade();
         }
+        //change the player
         this.shootingPlayer = newPlayer;
+        // add the stats to the new player
         this.shootingPlayer.addFreeThrowAttempt();
         if(this.made) {
             this.shootingPlayer.addFreeThrowMade();
         }
     }
 
-    actionJSON (): Object {
+    public actionJSON (): Object {
         return {
             "action": "freethrow",
             "actionId": this.actionId,
             "shootingPlayerId": this.shootingPlayer.playerId,
             "made": this.made,
-            
         }
     }
     
