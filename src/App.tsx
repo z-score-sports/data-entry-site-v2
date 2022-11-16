2e6776659a824d20f7864dc87c3094f1edcc1ab7
Lior Ben-David <liorbd@Outlook.com>
2022-11-16 18:31:13 -0500

    UI setup (#5)
    * Project setup
    
    * Setup panel divisions

--- src/App.tsx
+++ src/App.tsx
@@ -2,23 +2,24 @@ import React, { useContext } from 'react';
 import { observer } from "mobx-react-lite"
 import './App.css';
 import GameState from './state/GameState';
+import TopBar from './components/panels/TopBar';
+import LeftPanel from './components/panels/LeftPanel';
+import MainPanel from './components/panels/MainPanel';
+import RightPanel from './components/panels/RightPanel';
 
 function App() {
 
   const gameState = useContext(GameState)
 
 
-
   return (
     <div className="App">
-      This is our app.
-      <br/>
-      <button onClick={() => {gameState.increaseQuarter()}}>Click to increase quarter.</button>
-      {gameState.quarter}
-      <br/>
-      {gameState.awayTimeouts}
-      <br/>
-      {gameState.homeTimeouts}
+      <TopBar/>
+      <div className='MainSection'>
+        <LeftPanel/>
+        <MainPanel/>
+        <RightPanel/>
+      </div>
     </div>
   );
 }
