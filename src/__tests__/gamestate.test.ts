c14bea9a739de179c08c351481248ffc654ca49b
Michael Djorup <michael.djorup@gmail.com>
2022-11-16 11:10:03 -0500

    jest setup and example tests

---
+++ src/__tests__/gamestate.test.ts
@@ -0,0 +1,12 @@
+import {describe, expect, test} from '@jest/globals';
+
+
+describe('Gamestate Module', () => {
+    test("increment quarter", () => {
+        expect(1).toBe(1);
+    })
+
+    test("decrement quarter", () => {
+        expect(-1).toBe(-1);
+    })
+ })
