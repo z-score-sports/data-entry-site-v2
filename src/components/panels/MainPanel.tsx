c0c8726f16ed088f75ce465ce4dace811fbad7d0
mdjorup <michael.djorup@gmail.com>
2022-12-30 13:02:11 -0500

    remove main panel button

--- src/components/panels/MainPanel.tsx
+++ src/components/panels/MainPanel.tsx
@@ -6,19 +6,10 @@ import InputPanel from "../elements/InputPanel";
 function MainPanel() {
     const context = useContext(GameStateContext);
 
-    const handleClick = (event: React.MouseEvent) => {
-        if (event.type === "click") {
-            context.actionStack.addPossessionEnd();
-        }
-    };
-
     return (
         <div className="mPanel">
             <p>{context.actionStack.curPos}</p>
             This is the main panel
-            <button onKeyDown={() => {}} onClick={handleClick}>
-                Click to end pos
-            </button>
             <div>
                 <ul>{JSON.stringify(context.actionStack.toJSON())}</ul>
             </div>
