cc6c16a3868c51c9462ef2ce7c425e8ce0f240f7
Lior Ben-David <liorbd@Outlook.com>
2023-01-11 23:16:31 -0500

    setup routing

--- src/components/panels/TopBar.tsx
+++ src/components/panels/TopBar.tsx
@@ -1,6 +1,7 @@
 import { observer } from "mobx-react-lite";
 import { useContext, useState } from "react";
 import { GameStateContext } from "../../App";
+import {NavLink} from "react-router-dom"
 import "../../App.css";
 
 function TopBar() {
@@ -10,7 +11,12 @@ function TopBar() {
 
     return (
         <div className="tBar">
-
+            <img src = "logo.jpg"/>
+            <div className = "navbar">
+                <NavLink style = {{}}to="/">Dashboard</NavLink>
+                <NavLink to="/analytics">Analytics</NavLink>
+                <NavLink to="/roster">Roster Management</NavLink>
+            </div>
         </div>
     );
 }
