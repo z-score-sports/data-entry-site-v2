db106acea165a88ff27c059d581c8fceac7b3d22
Michael Djorup <68293826+mdjorup@users.noreply.github.com>
2023-01-18 09:14:36 -0500

    initial ui draft (#20)

--- src/components/elements/LogView.tsx
+++ src/components/elements/LogView.tsx
@@ -2,18 +2,31 @@ import { observer } from "mobx-react-lite";
 import { useContext } from "react";
 import { GameStateContext } from "../../App";
 
+import List from "@mui/material/List";
+import ListItem from "@mui/material/ListItem";
+import ListItemText from "@mui/material/ListItemText";
+
 function LogView() {
     const context = useContext(GameStateContext);
 
     return (
-        <div>
-            <h1>Action Log</h1>
-            <ul>
-                {context.actionStack &&
-                    context.actionStack
-                        .lastNActions(5)
-                        .map((action) => <li>{action.actionString}</li>)}
-            </ul>
+        <div className="logview">
+            <div className="logviewtitle">Action Log</div>
+            <List dense={true} className="logviewlist">
+                {context.actionStack
+                    .lastNActions(5)
+                    .map((actionRecord, index) => (
+                        <ListItem className="logviewitem ">
+                            <ListItemText
+                                primary={
+                                    actionRecord.index +
+                                    ". " +
+                                    actionRecord.action.actionString
+                                }
+                            />
+                        </ListItem>
+                    ))}
+            </List>
         </div>
     );
 }
