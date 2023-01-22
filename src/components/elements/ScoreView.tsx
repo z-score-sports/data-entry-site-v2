5cc61d0d5d4c7712bba893c05418c4c500c39bf2
Michael Djorup <68293826+mdjorup@users.noreply.github.com>
2023-01-22 14:08:25 -0500

    Scoreboard buttons (#33)
    * components on the screen with little styling
    
    * timeouts styling and functionality complete

--- src/components/elements/ScoreView.tsx
+++ src/components/elements/ScoreView.tsx
@@ -1,34 +1,124 @@
-import { useContext, useState } from "react";
-import { GameStateContext } from "../../App";
 import { observer } from "mobx-react-lite";
+import { useContext, useRef, useState } from "react";
+import { GameStateContext } from "../../App";
 import "./ScoreView.css";
 
 function ScoreView() {
     const context = useContext(GameStateContext);
+    const [homeTimeouts, setHomeTimeouts] = useState(4);
+    const [awayTimeouts, setAwayTimeouts] = useState(4);
 
+    const homeIncrease = useRef(null);
+    const awayIncrease = useRef(null);
+    const homeDecrease = useRef(null);
+    const awayDecrease = useRef(null);
 
-    return (<>
-        <div className = "sideLblSect">
-            <p>HOME{context.actionStack.curPos == 0 && <span className = "posCirc"></span>}</p>
-            <img src="homeLogo.jpg"/>
-        </div>
-        <div className = "midSect">
-            <p className = "qrtLbl">{context.scoreboard.getQuarter()}</p>
-            <p className = "scoreLbl">{context.scoreboard.homePoints} - {context.scoreboard.awayPoints}</p>
-            <div className = "foulCombinedSect">
-                <div className = "foulSect">
-                    <p className = "foulLbl">FOULS: 5</p>
+    const handleHomeIncrease = () => {
+        setHomeTimeouts(Math.min(homeTimeouts + 1, 4));
+        homeIncrease.current.blur();
+    };
+    const handleAwayIncrease = () => {
+        setAwayTimeouts(Math.min(awayTimeouts + 1, 4));
+        awayIncrease.current.blur();
+    };
+    const handleHomeDecrease = () => {
+        setHomeTimeouts(Math.max(0, homeTimeouts - 1));
+        homeDecrease.current.blur();
+    };
+    const handleAwayDecrease = () => {
+        setAwayTimeouts(Math.max(0, awayTimeouts - 1));
+        awayDecrease.current.blur();
+    };
+
+    return (
+        <>
+            <div className="sideLblSect">
+                <p>
+                    HOME
+                    {context.actionStack.curPos == 0 && (
+                        <span className="posCirc"></span>
+                    )}
+                </p>
+                <img src="homeLogo.jpg" />
+            </div>
+            <div className="midSect">
+                <p className="qrtLbl">{context.scoreboard.getQuarter()}</p>
+                <p className="scoreLbl">
+                    {context.scoreboard.homePoints} -{" "}
+                    {context.scoreboard.awayPoints}
+                </p>
+                <div className="foulCombinedSect">
+                    <div className="foulSect">
+                        <p className="foulLbl">FOULS: 5</p>
+                    </div>
+                    <div className="foulSect">
+                        <p className="foulLbl">FOULS: 5</p>
+                    </div>
                 </div>
-                <div className = "foulSect">
-                    <p className = "foulLbl">FOULS: 5</p>
+                <div className="scoreTimeoutSect">
+                    <div className="timeoutSect">
+                        <button
+                            ref={homeDecrease}
+                            className="timeoutButton"
+                            onClick={handleHomeDecrease}
+                        >
+                            -
+                        </button>
+                        {[...Array(4)].map((val, i) => (
+                            <div
+                                className={`circle ${
+                                    i + 1 <= homeTimeouts
+                                        ? "filled"
+                                        : "unfilled"
+                                }`}
+                            ></div>
+                        ))}
+                        <button
+                            ref={homeIncrease}
+                            className="timeoutButton"
+                            onClick={handleHomeIncrease}
+                        >
+                            +
+                        </button>
+                    </div>
+                    <div className="timeoutSect">
+                        <button
+                            ref={awayDecrease}
+                            className="timeoutButton"
+                            onClick={handleAwayDecrease}
+                        >
+                            -
+                        </button>
+                        {[...Array(4)].map((val, i) => (
+                            <div
+                                className={`circle ${
+                                    i + 1 <= awayTimeouts
+                                        ? "filled"
+                                        : "unfilled"
+                                }`}
+                            ></div>
+                        ))}
+                        <button
+                            ref={awayIncrease}
+                            className="timeoutButton"
+                            onClick={handleAwayIncrease}
+                        >
+                            +
+                        </button>
+                    </div>
                 </div>
             </div>
-        </div>
-        <div className = "sideLblSect">
-            <p>AWAY{context.actionStack.curPos == 1 && <span className = "posCirc"></span>}</p>
-            <img src="awayLogo.jpg"/>
-        </div>
-    </>)
+            <div className="sideLblSect">
+                <p>
+                    AWAY
+                    {context.actionStack.curPos == 1 && (
+                        <span className="posCirc"></span>
+                    )}
+                </p>
+                <img src="awayLogo.jpg" />
+            </div>
+        </>
+    );
 }
 
-export default observer(ScoreView)
+export default observer(ScoreView);
